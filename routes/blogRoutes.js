143acb3c4ebeb4df2068444a043d82841d934d46
Md. Saiful Islam <saiful.doict@gmail.com>
2024-11-03 13:14:05 +0600

    2nd route name defiled

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -4,9 +4,16 @@ const router = express.Router();
 const blogController = require('../controllers/blogController');
 
 // Define blog routes
+// Create Route
 router.post('/create-blog', blogController.createBlog);
+
+// Read Route
 router.get('/read-blog', blogController.readBlog);
+
+// Update Route
 router.put('/update-blog', blogController.updateBlog);
+
+// Delete Route
 router.delete('/delete-blog', blogController.deleteBlog);
 
 module.exports = router;
